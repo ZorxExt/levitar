2bea19e52dd3d9965a8697f1a039b995a2b374ef
ZorxExt <franciscosabatecastillo@gmail.com>
2023-05-04 17:26:01 -0300

    Update DivVideo.jsx

--- src/components/DivVideo.jsx
+++ src/components/DivVideo.jsx
@@ -10,7 +10,7 @@ export default function DivVideo() {
       />
       <div className="absolute top-0 left-0 w-screen h-screen object-cover bg-blue-950 opacity-30 bg-blend-overlay"></div>
       <span className="relative m-auto w-90">
-        <img src="public/levit-logo.png" alt="Levit.ar" className="px-8" />
+        <img src="levit-logo.png" alt="Levit.ar" className="px-8" />
         <h2
           className="text-4xl text-gray-50 text-center mt-12"
           style={{ letterSpacing: "8px" }}
