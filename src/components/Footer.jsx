fe8180279b245bb5bc5000e92229d6b30ec2d227
ZorxExt <franciscosabatecastillo@gmail.com>
2023-05-04 17:40:20 -0300

    l

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <footer className="my-16 md:mb-5">
-            <img src="public/levitar-ico.png" alt="" className="m-auto mb-10 h-10"/>
+            <img src="levitar-ico.png" alt="" className="m-auto mb-10 h-10"/>
             <p className="text-center text-xl" style={{fontFamily : "'Rubik', sans-serif",letterSpacing: "10px"}}>Despeguemos</p>
             <p className="text-center mt-5" style={{fontFamily : "'Rubik', sans-serif",letterSpacing: "10px"}}>By Francisco Sabaté</p>
         </footer>
